6d2b93b92e2dadf3c1c38e297e5c3103ddb33ddd
Juan Gonzalez Covarrubias <104181238+jgonzalezcov@users.noreply.github.com>
2022-09-02 13:33:29 -0400

    feat: The edition and elimination of objects in the array is incorporated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,11 +2,33 @@ import React from 'react'
 
 const Card = (props) => {
   return (
-    <div className="card">
-      <img className="img-card" src={props.srcs} alt={props.alt} />
-      <h3>Nombre: {props.name}</h3>
-      <p className="race">Raza: {props.category}</p>
-      <p className="email">E-mail: {props.email}</p>
+    <div className="container-card-view">
+      <div className="card-view-edit">
+        <img className="img-card-view" src={props.srcs} alt={props.alt} />
+        <h3>Nombre: {props.name}</h3>
+        <p className="race">Raza: {props.category}</p>
+        <p className="email">E-mail: {props.email}</p>
+        <div className="container-button-view">
+          <button
+            className="formIn view-button"
+            onClick={() => props.actionCard('edit')}
+          >
+            Editar
+          </button>
+          <button
+            className="formIn view-button"
+            onClick={() => props.actionCard('delete')}
+          >
+            Eliminar
+          </button>
+          <button
+            className="view-cancel"
+            onClick={() => props.actionCard('cancel-view')}
+          >
+            Cancelar
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
